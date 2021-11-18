9fa84368e465c53070089396c2369e345b5402d5
Maigurs Stalidzans <77847443+neimsaci@users.noreply.github.com>
2021-11-18 14:11:36 +0000

    Delete Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-# Creating two aliases that will allow vi and vim commands usage system wide
-New-Alias                  -Name vi        -Value     'C:\Program Files (x86)\Vim\vim82\vim.exe'
-New-Alias                  -Name vim       -Value     'C:\Program Files (x86)\Vim\vim82\vim.exe'
-
-# Enabling vim mode in PowerShell 
-Set-PSReadLineOption       -EditMode        vi
